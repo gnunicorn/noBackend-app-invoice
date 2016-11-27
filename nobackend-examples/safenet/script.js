c694bde8788a9d1f08da9172ce1f1b5a7d3334f8
Benjamin Kampmann <ben@gnunicorn.org>
2016-11-27 14:58:37 +0100

    add source doccos

--- nobackend-examples/safenet/script.js
+++ nobackend-examples/safenet/script.js
@@ -1,6 +1,6 @@
 // 
 // This file implements the "Dreamcode" of the
-// invoice app. It contains all the logic needed
+// invoice app for the SAFE Netwok. It contains all the logic needed
 // to make the static HTML app a full stack application,
 // including user accounts, data synchronization and
 // sending emails.
@@ -9,31 +9,47 @@
 // solutions, no implement its logic and making the app's
 // functionality real.
 // 
-// See also: http://nobackend.org/dreamcode.html
-// 
-
+// See also:
+//
+//  - http://www.safedev.org
+//  - http://tutorials.safedev.org
+//  - http://api.safedev.org
 // 
 // to allow for a nice, chainable API, each method returns
 // a promise. 
-// 
 
-// accept only on safe and/or localhost
+
+
+// Globals
+// Here we store the access token
+var accessToken;
+
+// and define our App information for authentication
+var app = {
+    name: "safejs-nobackend-invoice-example",
+    id: "net.maidsafe.examples.nobackend-invoice",
+    version: "2016-11-27", // using the date for version
+    vendor: "Benjamin Kampmann",
+    permissions: ["SAFE_DRIVE_ACCESS"]
+};
+
+
+
+// If we are not hosted on SAFE
+// and not running on localhost, we'll show the error message
 if (document.location.protocol !== 'safe:') {
-  if (document.location.hostname !== 'localhost') {
+  if (document.location.hostname !== 'localhost0') {
     alert("You need to run this within the SAFE browser!");
-    document.getElementsByClassName('invoiceSheet')[0].innerHTML = document.getElementById('redirectToSafe').innerHTML;
-    document.getElementsByClassName('footerBar')[0].innerHTML = '';
+    var errMsg = document.getElementById('redirectToSafe').innerHTML;
+    document.getElementsByClassName('invoiceSheet')[0].innerHTML = errMsg;
+    document.getElementById('invoiceBtn').innerHTML = '';
     throw("Unsupported Error")
   }
 }
 
 
-var _promise = function () {
-  var defer = $.Deferred()
-  defer.resolve.apply( defer, arguments )
-  return defer.promise()
-}
-
+// A helper function that wraps our promises
+// into a jquery-style Deferred as the API requires it
 function wrapPromise(prms) {
   var defer = $.Deferred();
   prms.then(defer.resolve,
@@ -42,31 +58,39 @@ function wrapPromise(prms) {
 }
 
 
-var accessToken;
-
 // 
 // data store dreamcode
 // 
+
+// In DreamCode, every object has at least a `type` and an `id`-field. WSo we'll use those
+// to organise our data, where we have one folder per `type` and then store each items as
+// stringified json file under `${type}/${id}.json`. Thus we can only use the usual NFS system
+// easily to provide the full API required
+
 var store = {
 
+  // entry point to the app: tries to fetch all data
   findAll : function(type) {
     function fetch() {
 
+    // let's fetch the folder of `type` and read all its files
+    // we assume there is nothing else in there than stuff we stored before
     return wrapPromise(safeNFS.getDir(accessToken, "" + type).then(function(dir) {
-      console.log("found dir", dir)
       return Promise.all(dir.files.map(function(file) {
-        console.log("found", file)
-        return safeNFS.getFile(accessToken, type + '/' + file.name, 'text').then(JSON.parse.bind(JSON));
+        // then return that list json-prased objects
+        return safeNFS.getFile(accessToken, type + '/' + file.name, 'text')
+            .then(JSON.parse.bind(JSON));
       }));
     }, function(err) {
-      // we just _assume_ the directory isn't existing yet
-      // create it
+      // we just _assume_ the directory isn't existing yet, so we create it
       console.warn(err);
       return safeNFS.createDir(accessToken, ""+ type).then(function() { return [] });
     }));
 
     }
 
+// so if it isn't logged in yet, we try to authenticate and then fetch or
+// otherwise fetch directly
     if (!accessToken) return account._signIn().then(fetch);
 
     return fetch();
@@ -74,36 +98,22 @@ var store = {
 
   // add a new or update an existing object
   save : function(object) {
-    console.log(object)
     if (!accessToken) return $.Deferred().reject("You need to login first");
     return wrapPromise(safeNFS.createOrUpdateFile(
       accessToken,
       object.type + '/' + object.id + ".json",
       JSON.stringify(object)
     ));
-  },
-
-  // remove object from store
-  remove : function(object) {
-    if (!accessToken) return $.Deferred().reject("You need to login first");
-    console.log("store.remove: ", object)
-    return _promise(object)
   }
 }
-
-var app = {
-    name: "safejs-nobackend-invoice-example",
-    id: "net.maidsafe.examples.nobackend-invoice",
-    version: "2016-11-27",
-    vendor: "Benjamin Kampmann",
-    permissions: ["SAFE_DRIVE_ACCESS"]
-};
 
 
 // 
 // account dreamcode
 // 
 var account = {
+  // internal function as we also want to call it from the
+  // Store
   _signIn: function () {
     return safeAuth.authorise(app).then(function(resp){
       accessToken = resp.token;
@@ -118,73 +128,6 @@ var account = {
   },
   signOut : function() {
     accessToken = null;
-    return _promise()
-  },
-  resetPassword : function( username ) {
-    return $.Deferred().reject("Not Supported");
-  },
-  changePassword : function( current_password, new_password ) {
-    return $.Deferred().reject("Not Supported");
-  },
-  changeUsername : function( current_password, new_username ) {
-    return $.Deferred().reject("Not Supported");
-  },
-  destroy : function() {
-    return $.Deferred().reject("Not Supported");
-  },
-
-  // to handle user account events
-  on : function(event, callback) {
-    console.log("account.on(\""+event+"\")", callback)
+    return $.Deferred().resolve(object).promise();
   }
 }
-
-// subscribe to account events
-account.on('signin', App.renderUserSignedIn)
-account.on('signout', App.renderUserSignedOut)
-account.on('unauthenticated', App.renderUserAuthenticationError)
-
-
-// 
-// remote / sync dreamcode
-// 
-var remote = {
-  on : function (event, callback) {
-    console.log("remote.on(\""+event+"\")", callback)
-  }
-}
-remote.on('add:invoice', App.addInvoiceFromRemote )
-remote.on('remove:invoice', App.removeInvoiceFromRemote)
-remote.on('update:invoice', App.updateInvoiceFromRemote)
-
-
-//
-// download & convert dreamcode
-// 
-var download = function ( what ) {
-  console.log('download: ', what)
-}
-var convert = function ( el ) {
-  console.log('convert', el)
-
-  var to = function( filename ) {
-    console.log('to', filename)
-    return "converted " + el.toString() + " to " + filename
-  }
-
-  return { to: to }
-}
-
-
-// 
-// email dreamcode
-// 
-var sendEmail = function(options) {
-  console.log('sendEmail: ', options)
-}
-
-
-// 
-// Question? Suggestions? Hugs? Please get in touch:
-// http://nobackend.org | https://twitter.com/nobackend
-// 
